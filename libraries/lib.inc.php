zsolt-kali/phppgadmin-cf

commit 36cdb2e40b34a2ca9e2e74db5748f8170f029aad

Fix a small PHP warning when Slony conf parameters are not set

libraries/lib.inc.php
@@ -249,7 +249,7 @@
 
 
 		// Load Slony if required
-		if ($_server_info['slony_support']) {
+		if (isset($_server_info['slony_support'])) {
 			include('./classes/plugins/Slony.php');
 			$slony = new Slony();
 		}

commit fa8edc2d0afa8bdcca83d9f99eeaf9a667141de1

Fix regression where slony menu is always shown.
Regression from patch 36cdb2e40b34a2ca9e2e74db5748f8170f029aad "Fix a
small PHP warning when Slony conf parameters are not set".

libraries/lib.inc.php
@@ -249,7 +249,7 @@
 
 
 		// Load Slony if required
-		if (isset($_server_info['slony_support'])) {
+		if (isset($_server_info['slony_support']) && $_server_info['slony_support']) {
 			include('./classes/plugins/Slony.php');
 			$slony = new Slony();
 		}